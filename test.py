5d533a0313fc4092388216fd31ce01817917bece
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 03:42:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+from makeprediction import makePrediction
+
 class Test():
     def __init__(self):
         self.name = 'test'
         
     def predict(self):        
-        return '123456'
+        classObj = makePrediction()
+        df = classObj.load_data()
+        df2 = classObj.scale_data(df)
+        classObj.get_test_data(df2)
+        classObj.load_model()
+        pred = classObj.make_prediction()
+        return pred
