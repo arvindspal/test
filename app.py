ed47b637e1f74382caa72f4d9f94c5c203ba6eab
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 03:22:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #return render_template('index.html')
-    return '<h1>hello ooooo </h1>'
+    return '<h1>hello ooooo utyuiuytyu </h1>'
 
 if __name__ == '__main__':
 	#app.run(debug=True)
