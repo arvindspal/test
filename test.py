7c42ef2e8425465cec032d75134da72c668691f2
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 04:16:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,5 @@ class Test():
         
     def predict(self):        
         classObj = makeprediction.makePrediction()
-        df = classObj.load_data()
-        df2 = classObj.scale_data(df)
-        classObj.get_test_data(df2)
-        classObj.load_model()
-        pred = classObj.make_prediction()
+        pred = classObj.arvind()
         return pred
