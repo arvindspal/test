181613785896bae8f6dc7d7cd923754c2cdc5e38
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 04:20:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,3 @@ class Test():
     def __init__(self):
         self.name = 'test'
         
-    def predict(self):        
-        classObj = makeprediction.makePrediction()
-        pred = classObj.arvind()
-        return pred
