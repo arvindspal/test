7471651d3e5ae867ea6d5f51172bbc58ee306532
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 03:28:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 from test import Test
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #return render_template('index.html')
-    return '<h1>hello ooooo utyuiuytyu </h1>'
+    return '<h1>hello ooooo utyuiuytyu 123456 </h1>'
 
 if __name__ == '__main__':
 	#app.run(debug=True)
