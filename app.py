2e363ea0b8ab63d2bd5c264ed26d2dd034f09cec
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 17:37:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ def home():
     #return render_template('index.html')
     return '<h1>hello ooooo utyuiuytyu 123456 </h1>'
 
-@app.route("/predict")
-def predict():
-    test1 = test.Test()
-    predictedvalue = test1.predict()
-    return predictedvalue
 
 if __name__ == '__main__':
 	#app.run(debug=True)
