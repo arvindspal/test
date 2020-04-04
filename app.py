edf67124476cb3de5dff70836f78cc6e44d2fb92
arvindspal <arvindpal.pal@gmail.com>
2020-04-04 17:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from test import Test
 
 app = Flask(__name__)
 
@@ -6,7 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #return render_template('index.html')
-    return '<h1>hello ooooo utyuiuytyu 123456 </h1>'
+    return '<h1>hello ooooo </h1>'
+
+@app.route("/predict")
+def predict():
+    #test = Test()
+    #predictedvalue = test.predict()
+    return 10024
 
 
 if __name__ == '__main__':
